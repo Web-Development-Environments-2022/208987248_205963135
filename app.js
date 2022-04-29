da68dc3b9d348a533403516de98d6150e6aef735
Maor <bitondor@post.bgu.ac.il>
2022-04-29 14:23:41 +0300

    adding login and registation pages

--- app.js
+++ app.js
@@ -170,3 +170,22 @@ function UpdatePosition() {
 		Draw();
 	}
 }
+
+// let loginForm = document.getElementById("login");
+// let registerForm = document.getElementById("register");
+// let btn = document.getElementById("btn");
+// let form = document.getElementById("form");
+
+// function register(){
+// 	loginForm.style.left = "-400px";
+// 	registerForm.style.left = "50px";
+// 	btn.style.left = "110px";
+// 	form.style.height = "600px"
+// }
+
+// function login(){
+// 	loginForm.style.left = "50px";
+// 	registerForm.style.left = "450px";
+// 	btn.style.left = "0";
+// 	form.style.height = "480px"
+// }
