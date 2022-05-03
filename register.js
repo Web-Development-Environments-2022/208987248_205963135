c5efa632028b6dbe0c1a902ad55197a20fd01d23
Maor <bitondor@post.bgu.ac.il>
2022-05-03 14:05:39 +0300

    Adding register required and validation constranit

--- register.js
+++ register.js
@@ -6,21 +6,48 @@ function validateRegistration(){
     let regPassword = document.getElementById("regPass")
 }
 
-function validateUsername() {
-    let usernameValue = $('#regUser').val();
-    if (usernameValue.length == '') {
-    $('#usercheck').show();
-        usernameError = false;
-        return false;
+$("form[name='registration']").validate({
+    // Specify validation rules
+    rules: {
+      // The key name on the left side is the name attribute
+      // of an input field. Validation rules are defined
+      // on the right side
+      regUser: {
+        required: true
+      },
+      regPass: {
+        required: true,
+        minlength: 6,
+        pattern: "(?=.*\d)(?=.*[A-Za-z])"
+      },
+      regEmail: {
+        required: true,
+        // Specify that email should be validated
+        // by the built-in "email" rule
+        email: true
+      },
+      regName: {
+        required: true,
+        pattern: "(?=.*[A-Za-z])"
+      },
+      regBirth: {
+        required: true,
+          date: true
+      }
+    },
+    // Specify validation error messages
+    messages: {
+      firstname: "Please enter your firstname",
+      lastname: "Please enter your lastname",
+      password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 5 characters long"
+      },
+      email: "Please enter a valid email address"
+    },
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+    submitHandler: function(form) {
+      form.submit();
     }
-    else if((usernameValue.length < 3)||(usernameValue.length > 10)) {
-        $('#usercheck').show();
-        $('#usercheck').html
-        ("**length of username must be between 3 and 10");
-        usernameError = false;
-        return false;
-    }
-    else {
-        $('#usercheck').hide();
-    }
-  }
+  });
