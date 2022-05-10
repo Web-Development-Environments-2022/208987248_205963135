5ad77da7a566a93e32535084375709f4be6a120a
Maor <bitondor@post.bgu.ac.il>
2022-05-10 22:40:04 +0300

    start fixing ghosts problems

--- ghost.js
+++ ghost.js
@@ -1,9 +1,10 @@
 class Ghost{
-    constructor(color, centerGhost){
+    constructor(color, colPosition, rowPosition){
         this.color = color;
         this.direction = "RIGHT";
-        this.centerGhost = centerGhost;
-        this.prevLocation = "Empty";
+        this.prevCellValue = "Empty";
+        this.colPosition = colPosition;
+        this.rowPosition = rowPosition;
         this.orangeGhostRight = new Image();
         this.orangeGhostRight.src = "Images/orange_ghost_right.jpg";
         this.orangeGhostLeft = new Image();
@@ -23,56 +24,97 @@ class Ghost{
         // this.intervalGhost = setInterval(this.drawGhost, 400)
     }
 
-    drawGhost(){
+    drawGhost(centerGhost){
         // console.log(this)
         // console.log("after")
         if(this.color == "ORANGE"){
             if(this.direction == "RIGHT"){
                 // var orangeGhostRight = new Image();
                 // orangeGhostRight.src = "Images/orange_ghost_right.jpg";
-                context.drawImage(this.orangeGhostRight, this.centerGhost.x-15, this.centerGhost.y-15, 30, 30);
+                context.drawImage(this.orangeGhostRight, centerGhost.x-15, centerGhost.y-15, 30, 30);
             }
             else if(this.direction == "LEFT"){
                 // var orangeGhostLeft = new Image();
                 // orangeGhostLeft.src = "Images/orange_ghost_left.jpg";
-                context.drawImage(this.orangeGhostLeft, this.centerGhost.x-15, this.centerGhost.y-15, 30, 30);
+                context.drawImage(this.orangeGhostLeft, centerGhost.x-15, centerGhost.y-15, 30, 30);
             }
         }
         else if(this.color == "RED"){
             if(this.direction == "RIGHT"){
                 // var redGhostRight = new Image();
                 // redGhostRight.src = "Images/red_ghost_right.jpg";
-                context.drawImage(this.redGhostRight, this.centerGhost.x-15, this.centerGhost.y-15, 30, 30);
+                context.drawImage(this.redGhostRight, centerGhost.x-15, centerGhost.y-15, 30, 30);
             }
             else if(this.direction == "LEFT"){
                 // var redGhostLeft = new Image();
                 // redGhostLeft.src = "Images/red_ghost_left.jpg";
-                context.drawImage(this.redGhostLeft, this.centerGhost.x-15, this.centerGhost.y-15, 30, 30);
+                context.drawImage(this.redGhostLeft, centerGhost.x-15, centerGhost.y-15, 30, 30);
             }
         }
         else if(this.color == "PINK"){
             if(this.direction == "RIGHT"){
                 // var pinkGhostRight = new Image();
                 // pinkGhostRight.src = "Images/pink_ghost_right.png";
-                context.drawImage(this.pinkGhostRight, this.centerGhost.x-15, this.centerGhost.y-15, 30, 30);
+                context.drawImage(this.pinkGhostRight, centerGhost.x-15, centerGhost.y-15, 30, 30);
             }
             else if(this.direction == "LEFT"){
                 // var pinkGhostLeft = new Image();
                 // pinkGhostLeft.src = "Images/pink_ghost_left.png";
-                context.drawImage(this.pinkGhostLeft, this.centerGhost.x-15, this.centerGhost.y-15, 30, 30);
+                context.drawImage(this.pinkGhostLeft, centerGhost.x-15, centerGhost.y-15, 30, 30);
             }
         }
         else if(this.color == "GREEN"){
             if(this.direction == "RIGHT"){
                 // var greenGhostRight = new Image();
                 // greenGhostRight.src = "Images/green_ghost_right.jpg";
-                context.drawImage(this.greenGhostRight, this.centerGhost.x-15, this.centerGhost.y-15, 30, 30);
+                context.drawImage(this.greenGhostRight, centerGhost.x-15, centerGhost.y-15, 30, 30);
             }
             else if(this.direction == "LEFT"){
                 // var greenGhostLeft = new Image();
                 // greenGhostLeft.src = "Images/green_ghost_left.jpg";
-                context.drawImage(this.greenGhostLeft, this.centerGhost.x-15, this.centerGhost.y-15, 30, 30);
+                context.drawImage(this.greenGhostLeft, centerGhost.x-15, centerGhost.y-15, 30, 30);
             }
         }
+    }
+
+    calculateDistance(pacmanX, pacmanY){
+        let distanceArray = new Array();
+        let minValue = Infinity;
+        let minRowPostion = this.rowPosition;
+        let minColPostion = this.colPosition;
+        if(board[this.colPosition][this.rowPosition-1] != "Wall"){
+           let upValue = Math.sqrt(Math.pow(pacmanX-this.colPosition, 2) + Math.pow(pacmanY-(this.rowPosition-1), 2));
+           if(upValue < minValue){
+               minValue = upValue;
+               minRowPostion = this.rowPosition-1;
+               minColPostion = this.colPosition;
+           }
+        }
+        if(board[this.colPosition][this.rowPosition+1] != "Wall"){
+            let downValue = Math.sqrt(Math.pow(pacmanX-this.colPosition, 2) + Math.pow(pacmanY-(this.rowPosition+1), 2))
+            if(downValue < minValue){
+                minValue = downValue;
+                minRowPostion = this.rowPosition+1;
+                minColPostion = this.colPosition;
+            }
+        }
+        if(board[this.colPosition+1][this.rowPosition] != "Wall"){
+            let rightValue = Math.sqrt(Math.pow(pacmanX-(this.colPosition+1), 2) + Math.pow(pacmanY-this.rowPosition, 2))
+            if(rightValue < minValue){
+                minValue = rightValue;
+                minRowPostion = this.rowPosition;
+                minColPostion = this.colPosition+1;
+            }
+        }
+        if(board[this.colPosition-1][this.rowPosition] != "Wall"){
+            let leftValue = Math.sqrt(Math.pow(pacmanX-(this.colPosition-1), 2) + Math.pow(pacmanY-this.rowPosition, 2))
+            if(leftValue < minValue){
+                minValue = leftValue;
+                minRowPostion = this.rowPosition;
+                minColPostion = this.colPosition-1;
+            }
+        }
+        console.log(minValue);
+        return [minColPostion, minRowPostion]; // return the cell that will bring the ghost closest to the pacman
     }
 }
