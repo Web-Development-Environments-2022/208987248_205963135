c96d3f409769d32f2e13b5b78418d08e20de8bed
danahoh <danahoh3@gmail.com>
2022-05-18 23:26:31 +0300

    save again

--- sweetSourCandy.js
+++ sweetSourCandy.js
@@ -1,11 +1,7 @@
 class SweetSourCandy{
     constructor(){
         this.image = new Image();
-<<<<<<< HEAD
         this.image.src = "Images/candy3.png";
-=======
-        this.image.src = "Images/candy.png";
->>>>>>> dff9a1e484588e8dccd770598cfb83a46e5fe8b5
         this.score = 0;
     }
 
